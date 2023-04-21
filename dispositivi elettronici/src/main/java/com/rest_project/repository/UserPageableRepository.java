9cb4a11ca099daaef47d524caadeeab4cda080e1
OmarCovolo <119486782+OmarCovolo@users.noreply.github.com>
2023-04-21 17:36:02 +0200

    Delete dispositivi elettronici directory

--- dispositivi elettronici/src/main/java/com/rest_project/repository/UserPageableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rest_project.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.rest_project.model.User;
-
-public interface UserPageableRepository extends PagingAndSortingRepository<User, Long> {
-
-}
