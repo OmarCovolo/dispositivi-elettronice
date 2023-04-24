0c50ff109ef521069a0dc8cd8e7f9b8d0c50a0e5
OmarCovolo <119486782+OmarCovolo@users.noreply.github.com>
2023-04-24 09:44:28 +0200

    Delete dispositivi elettronici copia 2 directory

--- dispositivi elettronici copia 2/src/main/java/com/rest_project/repository/DevicePageableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rest_project.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.rest_project.model.Device;
-
-public interface DevicePageableRepository extends PagingAndSortingRepository<Device, Long> {
-
-}
